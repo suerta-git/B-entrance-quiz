96de422bf2f5216cec9ff88dce75979016e4cd5d
Suerta <suerta__@hotmail.com>
2020-10-27 20:02:01 +0800

    [Zhang Jindong] feat: should support CORS

--- src/main/java/com/thoughtworks/capability/gtb/entrancequiz/controller/ConnectionTestController.java
+++ src/main/java/com/thoughtworks/capability/gtb/entrancequiz/controller/ConnectionTestController.java
@@ -1,10 +1,12 @@
 package com.thoughtworks.capability.gtb.entrancequiz.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:1234")
 public class ConnectionTestController {
     @GetMapping("/connection-test")
     public ResponseEntity<Void> testConnection() {
